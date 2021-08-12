3a3cafce391d1096397edee10088a43b1272f80a
PatriciaRuffino <sindelformr@gmail.com>
2021-08-12 19:51:41 -0300

    atualizacao

--- Aula02Mesa/js/app.js
+++ Aula02Mesa/js/app.js
@@ -3,7 +3,7 @@ let jogar = confirm("Vamos jogar!")
 if(jogar){
     let jogadorPontos =0
     let computadorPontos =0
-
+        
     while(jogadorPontos < 3 && computadorPontos <3){
         let escolhas =["pedra", "papel", "tesoura"]
         let computador = escolhas[Math.floor(Math.random()*escolhas.length)]
@@ -15,23 +15,23 @@ if(jogar){
             case "pedrapedra":
             case "tesouratesoura":
             case "papelpapel":
-                alert(`Sua escolha: ${jogadorConfim}. Minha escolha: ${computador}. Empatamos!`)
+                alert(`Sua escolha: ${jogadorConfim}. Minha escolha: ${computador}. Empatamos!😌`)
             break;
             case "pedratesoura":
             case "tesourapapel":
             case "papelpedra":
                 computadorPontos++
-                    alert(`Sua escolha: ${jogadorConfim}. Minha escolha: ${computador}. Eu ganhei!. 
+                    alert(`Sua escolha: ${jogadorConfim}. Minha escolha: ${computador}. Eu ganhei!😃. 
                     Sua pontuação: ${jogadorPontos}. Minha pontuação ${computadorPontos}`)
             break;
             default:
                 jogadorPontos++
-                    alert(`Sua escolha: ${jogadorConfim}. Minha escolha: ${computador}. Você ganhou! Sua pontução: ${jogadorPontos}. Minha pontuação ${computadorPontos}`)
+                    alert(`Sua escolha: ${jogadorConfim}. Minha escolha: ${computador}. Você ganhou!😤 Sua pontução: ${jogadorPontos}. Minha pontuação ${computadorPontos}`)
          }
         }
         if(jogadorPontos ==3){
-            alert("Parabéns, você ganhou!")
+            alert("Parabéns, você ganhou!🥳")
         }else if(computadorPontos ==3){
             alert("Eu ganhei!!!")
         }else alert("Nos vemos na próxima!")
-    }else alert("Nos vemos na próxima!")
+    }else alert("Nos vemos na próxima!🥺")
